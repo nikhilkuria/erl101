2a58d00c1fb6cefb2a058e28dcbd82c2597c3175
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2016-07-13 00:13:40 +0530

    loving erlang

---
+++ basics/reverseList.erl
@@ -0,0 +1,11 @@
+-module(reverseList).
+-export([reverse/1]).
+
+reverse(List) ->
+    reverse(List, []).
+
+reverse([Head | Rest], Reversed_List) ->
+  reverse(Rest, [Head | Reversed_List]);
+
+reverse([], Reversed_List) ->
+  Reversed_List.
